9a7f5bfe244802d23a1e351418dfb59caa0762a5
alex <1alexispetropoulos@gmail.com>
2024-02-14 23:46:02 +0100

    création de la page add.php

--- PHP/add.php
+++ PHP/add.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../dist/output.css">
     <title></title>
 </head>
-<body>
-    <a href="http://localhost/projet-13-fevrier/test.php">Contact support again</a>
-    <a href="http://localhost/projet-13-fevrier/PHP/update.php">Modify your response</a>
+
+<body class="bg-gray-100 flex items-center justify-center h-screen">
+    <div class="mx-auto max-w-md">
+        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <img src="../img/add.png" alt="someone on his computer" class="max-w-full h-auto"></img>
+            <div class="text-gray-700 text-center mt-5 mb-5 text-lg font-roboto">Thank you for your message! We've received it and will process your request shortly. Your satisfaction is our top priority.</div>
+            <div class="flex justify-center">
+            <button class="bg-blue-600 p-2 text-white rounded hover:bg-blue-700"><a href="http://localhost/Hackers-Poulette/index.php">Contact support again</a></button>
+            <button class="bg-red-600 p-2 text-white rounded ml-4 hover:bg-red-700"><a href="http://localhost/Hackers-Poulette/PHP/update.php">Modify your response</a></button>
+</div>
+        </div>
+    </div>
+
+
 </body>
+
 </html>
